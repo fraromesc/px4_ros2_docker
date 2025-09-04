c7bea9771bf8b623df74116903363f82f407666d
fraromesc <fraromesc@gmail.com>
2025-09-04 11:22:35 +0200

    ++ container_run.sh

---
+++ container_run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export CONTAINER_NAME="px4_ros2_cont"
+export IMAGE_NAME="px4_ros2"
+
+# Permitir al contenedor acceder al servidor X
+xhost +local:docker
+
+# Exporta correctamente el DISPLAY del host al contenedor
+export DISPLAY=${DISPLAY:-:0}
+
+# Usa tu HOME real en lugar de /root si no estás corriendo como root fuera del contenedor
+PROJECT_DIR="/"
+PROJECT_DIST="/"
+
+if [ "$(docker ps -qaf name=$CONTAINER_NAME)" = "" ]; then
+    echo 'Container not found, creating it ...'
+
+    docker run -it \
+        --name $CONTAINER_NAME \
+        --privileged \
+        --env DISPLAY=$DISPLAY \
+        --env QT_X11_NO_MITSHM=1 \
+        --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        --volume $PROJECT_DIR:$PROJECT_DIST \
+        --net=host \
+        $IMAGE_NAME bash
+
+    echo 'Container created.'
+else
+    if [ "${1}" = "restart" ] && ! docker stop $CONTAINER_NAME > /dev/null; then
+        echo 'Error while stopping the container, exiting now ...'
+        return 1
+    fi
+
+    if ! docker start $CONTAINER_NAME > /dev/null; then
+        echo 'Error while starting the container, exiting now ...'
+        return 1
+    fi
+
+    echo 'px4_docker found and running, executing a shell ...'
+    docker exec -it $CONTAINER_NAME bash --login
+fi
